f9618e742333413444e2a5d7e424249f5c76be3d
fatmambo33 <51465707+fatmambot33@users.noreply.github.com>
2023-06-03 20:03:27 +0200

    Update main.py
    more chatGPT documentation

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-# %%
-# Importing necessary libraries
 import json
 import logging
-
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 from pandas.api.types import is_numeric_dtype
 
-
-# Defining the netX class
 class netX():
-    # This class will represent the Graph
+    """
+    Class for representing and manipulating network data.
+    """
+    
     G:nx.Graph
     
     def __init__(self,df_source: pd.DataFrame,
@@ -19,63 +17,61 @@ class netX():
                  target_column:str="target",
                  weight_column:str="weight",
                  max_nodes:int=50):
-        # Initialization function to create a netX object
+        """
+        Initialize netX instance with network data.
 
-        # Check if source_column exists in dataframe
+        :param df_source: DataFrame containing network data.
+        :param source_column: Name of source nodes column in df_source.
+        :param target_column: Name of target nodes column in df_source.
+        :param weight_column: Name of edges weight column in df_source.
+        :param max_nodes: Maximum number of nodes to include in the graph.
+        """
+        
+        # Input validation
         if source_column not in df_source.columns:
             raise ValueError('missing source column')
-        
-        # Check if target_column exists in dataframe
         if target_column not in df_source.columns:
             raise ValueError('missing target column')
-        
-        # Check if weight_column exists and is numeric
         if weight_column not in df_source.columns:
             raise ValueError('missing weight column')
         elif not is_numeric_dtype(df_source[weight_column]):
             raise ValueError('weight column is not numeric')
-
-        # Drop duplicates and rename columns
-        df= df_source.drop_duplicates()
-        df.rename(columns={source_column: "source", target_column: "target",weight_column: "weight"})
         
-        # Create symmetric dataframe and drop duplicates
+        # Preprocessing
+        df= df_source.drop_duplicates()
+        df.rename(columns={source_column: "source", "target_column": "target","weight_column": "weight"})
         df = pd.concat([df, pd.DataFrame(
             {"source": df["target"], "target":df["source"], "weight":df["weight"]})], ignore_index=True).drop_duplicates()
-
-        # Group by source, aggregate on weight and get the first max_nodes
+        
         df_node = df.groupby('source').agg({'weight': 'sum'}).sort_values(by="weight").head(max_nodes)
         
         self.mean = df.agg({'weight': 'mean'})
-
-        # Creating nodes
         nodes = [(index, {"size": row["weight"]})
                 for index, row in df_node.iterrows()]
-        # Creating edges
         edges = [(row["source"], row["target"], int(row["weight"]))
                 for index, row in df.iterrows() if row["source"] in nodes and row["target"] in nodes]
-
-        # Creating a Graph
-        self.G = nx.Graph()
         
+        # Graph initialization
+        self.G = nx.Graph()
         logging.info(f'Adding {len(nodes)} nodes')
         self.G.add_nodes_from(nodes)
-        
         logging.info(f'Adding {len(edges)} edges')
         self.G.add_weighted_edges_from(edges)
 
     def node_size(self):
-        # Function to calculate node size based on weight
+        """
+        Calculate and return node sizes based on weight.
+        """
         return [
         v/self.mean * 10 for v in nx.get_node_attributes(self.G, "size").values()]
 
     def draw(self):
-        # Function to draw the graph
-
-        # Defining position of nodes and edges
+        """
+        Draw the graph using matplotlib.
+        """
         pos=nx.spring_layout(self.G,scale=2,k=5)
 
-        # Drawing nodes with specific options
+        # nodes
         nodes_options = {"node_size": self.node_size(),
                         "node_color": "tab:red",
                         "alpha": 0.9}
@@ -83,14 +79,13 @@ class netX():
                             pos,
                             **nodes_options)
 
-        # Drawing labels with specific options
+        # labels
         labels_options = {"alpha": 0.9}
         nx.draw_networkx_labels(
             self.G,
             pos,
             **labels_options)
-        
-        # Drawing edges with specific options
+        # edges
         edges_options = {
                         "alpha": 0.9,
                         "width": 8}
@@ -99,21 +94,18 @@ class netX():
             pos,
             **edges_options)
 
-        # Setting margins for the axes so that nodes aren't clipped
+        # Set margins for the axes so that nodes aren't clipped
         ax = plt.gca()
         ax.margins(0.20)
         plt.axis("off")
         plt.show()
 
-# Main function to test the class functionality
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-
-    # Opening JSON file and loading it into a DataFrame
+    
+    # Opening JSON file
     with open('data.json') as f:
         df = pd.DataFrame(json.load(f))
 
-    # Creating an object of netX class and drawing the graph
     graph=netX(df)
     graph.draw()
-# %%
